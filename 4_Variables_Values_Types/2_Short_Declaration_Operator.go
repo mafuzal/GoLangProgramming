44db6c101514f2f660c5552013a4b19f9304bc06
mafuzalhoque <mafuzalhoque@gmail.com>
2022-01-26 22:41:06 -0500

    Short Declaration Operator

---
+++ 4_Variables_Values_Types/2_Short_Declaration_Operator.go
@@ -0,0 +1,16 @@
+//Short Declaration Operator  :=
+//Allows us to declare and assign at the same time
+package main
+
+import "fmt"
+
+func main() {
+	x := 100
+	fmt.Println(x)
+	x = 200
+	fmt.Println(x)
+	y := 100 + 70
+	fmt.Println(y)
+	z := "Saturn,  Sky"
+	fmt.Println(z)
+}
